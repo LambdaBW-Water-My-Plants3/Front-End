169375ae91d2551694616567f4986bb3d5ca1472
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-09-21 15:25:41 -0500

    added validation

--- water_my_plants/src/components/Login.js
+++ water_my_plants/src/components/Login.js
@@ -1,10 +1,27 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
+import * as yup from 'yup'
+import schema from './login-validation/formSchema'
+
+const initialValues = {
+    username: '',
+    password: '',
+}
+
+const initialErrors = {
+    username: '',
+    password: '',
+}
 
 
+const initialDisabled = true
 
 const Login = (props) => {
-    const [credentials, setCredentials] = useState({username: '', password: ''});
+    const [credentials, setCredentials] = useState(initialValues);
+    const [credentialErrors, setCredentialErrors] = useState(initialErrors)
+    const [disabled, setDisabled] = useState(initialDisabled)
+
+
      const login = e => {
        e.preventDefault();
        axios.post(' link here', `grant_type=password&username=${credentials.username}&password=${credentials.password}`, {
@@ -20,15 +37,50 @@ const Login = (props) => {
          props.history.push('/');
        })
      }
+     const inputChange = (name, value) => {
+         validate(name, value)
+         setCredentials({
+             ...credentials, [name]: value
+         })
+     }
+     const onChange = evt => {
+         const {name, value} = evt.target
+         inputChange(name, value)
+     }
+
+     const validate = (name, value) => {
+        yup
+        .reach(schema, name)
+        .validate(value)
+        .then(valid =>{
+            setCredentialErrors({
+                ...credentialErrors, [name]: ""
+            })
+        })
+        .catch(error =>{
+            setCredentialErrors({
+                ...credentialErrors, [name]:error.errors[0]
+            })
+        })
+     }
+
+     useEffect(()=>{
+         schema.isValid(credentials)
+         .then(valid =>{
+             setDisabled(!valid)
+         })
+     },[credentials])
      return(
          <div className = "loginContainer">
-            <form>
+            <form className = "loginForm" onSubmit={login}>
                 <h1>Welcome Back</h1>
             <div className = "username">
                 <label>Username:
                     <input
                         type="text"
                         name="username"
+                        value = {credentials.username}
+                        onChange = {onChange}
                     />
                 </label>
             </div>
@@ -37,11 +89,17 @@ const Login = (props) => {
                     <input
                         type= "password"
                         name="password"
+                        value={credentials.password}
+                        onChange = {onChange}
                     />
                 </label>
             </div> 
+            <div className = "errors">
+                <div>{credentialErrors.username}</div>
+                <div>{credentialErrors.password}</div>
+            </div>
             <div className="submit">
-                <button>Login</button>
+                <button disabled={disabled}>Login</button>
             </div>   
             </form>
          </div>
