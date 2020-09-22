14300ea1e1869ac0afd16d2706fb0dec634e548f
KindredSoul <jabriltb2@gmail.com>
2020-09-22 11:48:58 -0500

    changes before merge

--- water_my_plants/src/components/SignUp.js
+++ water_my_plants/src/components/SignUp.js
@@ -2,26 +2,24 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { Box, Button, createMuiTheme, TextField } from "@material-ui/core";
 import { withStyles, ThemeProvider } from "@material-ui/styles";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers";
+// import * as yup from "yup";
+// import { yupResolver } from "@hookform/resolvers";
 import bgImg from "../images/littlePlant.jpg";
 
-const schema = yup.object().shape({
-	username: yup.string().required("Please enter a username"),
-	password: yup.string().required("Please enter a password"),
-	phone_number: yup
-		.string()
-		.required("A valid phone number is required")
-		.matches(
-			/^[(]{0,1}[0-9]{3}[)]{0,1}[-]{1}[0-9]{3}[-]{1}[0-9]{4}$/g,
-			"Invalid number"
-		),
-});
+// const schema = yup.object().shape({
+// username: yup.string().required("Please enter a username"),
+// password: yup.string().required("Please enter a password"),
+// phone_number: yup
+// 	.string()
+// 	.required("A valid phone number is required")
+// 	.matches(
+// 		/^[(]{0,1}[0-9]{3}[)]{0,1}[-]{1}[0-9]{3}[-]{1}[0-9]{4}$/g,
+// 		"Invalid number"
+// 	),
+// });
 
 const SignUp = () => {
-	const { register, handleSubmit, errors } = useForm({
-		resolver: yupResolver(schema),
-	});
+	const { register, handleSubmit, errors } = useForm();
 
 	const onSubmit = (data) => console.log(data);
 
@@ -49,7 +47,7 @@ const SignUp = () => {
 			height: "80vh",
 			overflowY: "scroll",
 			padding: [[0, 20, 50]],
-			backgroundImage: `linear-gradient(rgba(155,155,155,0.6), rgba(155,155,155,0.2)), url(${bgImg})`,
+			backgroundImage: `linear-gradient(rgba(255,255,255,0.6), rgba(255,255,255,0.2)), url(${bgImg})`,
 			backgroundPosition: "center",
 		},
 	})(Box);
@@ -62,45 +60,58 @@ const SignUp = () => {
 					<TextField
 						name="username"
 						label="Username"
-						inputRef={register}
+						inputRef={register({ required: true, minLength: 2 })}
 						required
 						color="primary"
 						fullWidth
 						margin="normal"
 						variant="outlined"
 						placeholder="Enter a username"
+						helperText={
+							errors.username?.type === "minLength" &&
+							"Username is too short! Please enter a valid username."
+						}
 					/>
-					<p>{errors.username?.message}</p>
 					<TextField
 						name="password"
 						label="Password"
 						type="password"
-						inputRef={register}
+						inputRef={register({ required: true })}
 						required
 						color="primary"
 						fullWidth
 						margin="normal"
 						variant="outlined"
 						placeholder="Enter a password"
+						helperText={
+							errors.password?.type === "minLength" &&
+							"Password is too short! Please enter a validpassword."
+						}
 					/>
-					<p>{errors.password?.message}</p>
 					<TextField
 						name="phone_number"
 						label="Phone Number"
 						type="tel"
-						inputRef={register}
+						inputRef={register({
+							required: true,
+							pattern: /^[(]{0,1}[0-9]{3}[)]{0,1}[-]{1}[0-9]{3}[-]{1}[0-9]{4}$/g,
+						})}
 						required
 						color="primary"
 						fullWidth
 						margin="normal"
 						variant="outlined"
 						placeholder="555-555-5555"
+						helperText={
+							errors.phone_number?.type === "pattern" &&
+							"Please enter a valid phone number"
+						}
 					/>
-					<p>{errors.phone_number?.message}</p>
 					<Button fullWidth variant="outlined" color="secondary" type="submit">
 						Submit
 					</Button>
 				</form>
+				<p>Already a member? Sign In here!</p>
 			</SignUpWrapper>
 		</ThemeProvider>
 	);
