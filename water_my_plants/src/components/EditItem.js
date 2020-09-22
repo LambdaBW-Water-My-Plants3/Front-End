41c80c36d0c6c45ad42d7d81d014abbaed4b6714
unknown <brettmcadams96@gmail.com>
2020-09-21 20:47:09 -0700

    finished form

--- water_my_plants/src/components/EditItem.js
+++ water_my_plants/src/components/EditItem.js
@@ -1,33 +1,110 @@
-import React, { useState, useEffect } from 'react'
-import { axiosWithAuth } from '../utils/axiosWithAuth'
-import axios from 'axios'
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from "@material-ui/icons/Edit";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+}));
 
 const initialPlantValues = {
-    nickname: "",
-    species: "",
-    waterfrequency: ""
-}
+  nickname: "",
+  species: "",
+  waterfrequency: "",
+};
 
-function EditItem() {
+function EditItem({ plants, updatePlants }) {
+  // styles
 
-    // State
-    const [editing, setEditing] = useState(false)
-    const [plantToEdit, setPlantToEdit] = useState(initialPlantValues)
+  const classes = useStyles();
 
-    const editPlant = (currentPlant) => {
-        setEditing(true)
-        setPlantToEdit(currentPlant)
-    }
+  // State
 
-    const savePlant = (e) => {
-        e.preventDefault()
-    }
+  const [editing, setEditing] = useState(false);
+  const [plantToEdit, setPlantToEdit] = useState(initialPlantValues);
+  // action handlers
 
-    return (
-        <div>
-            <p>edit list</p>
+  const editPlant = (currentPlant) => {
+    setEditing(true);
+    // setPlantToEdit(currentPlant);
+  };
+
+  const savePlant = (e) => {
+    e.preventDefault();
+    // put request
+  };
+
+  const deletePlant = (plant) => {
+    // delete request
+  };
+
+  const change = e => {
+    e.persist()
+    setPlantToEdit({...plantToEdit, [e.target.name]: e.target.value})
+  }
+
+  return (
+    <div>
+      {plants.map((plant) => (
+        <div
+          style={{ display: "flex", justifyContent: "space-between" }}
+          key={plant.id}
+        >
+          <p>{plant.nickname}</p>
+          <div style={{display: 'flex'}}>
+            <div onClick={editPlant} >
+              <IconButton aria-label="delete" color="primary">
+                <EditIcon fontSize="small" />
+              </IconButton>
+            </div>
+            <div>
+              <IconButton aria-label="delete" color="primary">
+                <DeleteIcon fontSize="small" />
+              </IconButton>
+            </div>
+          </div>
         </div>
-    )
+      ))}
+      {editing && (
+        <form onSubmit={savePlant}>
+          <legend>Edit Color:</legend>
+          <label>
+            Species:
+            <input
+              onChange={change}
+              value={plantToEdit.species}
+            />
+          </label>
+          <label>
+            Nickname:
+            <input
+              onChange={change}
+              value={plantToEdit.nickname}
+            />
+          </label>
+          <label>
+            Water:
+            <input
+              onChange={change}
+              value={plantToEdit.waterfrequency}
+            />
+          </label>
+
+          <div className="button-row">
+            <button type="submit">save</button>
+            <button onClick={() => setEditing(false)}>cancel</button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
 }
 
-export default EditItem
+export default EditItem;
--- water_my_plants/src/components/PlantPage.js
+++ water_my_plants/src/components/PlantPage.js
@@ -1,29 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import EditItem from './EditItem'
-import Plants from './Plants'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import EditItem from "./EditItem";
+import Plants from "./Plants";
+
+import "../css/plantpage.css";
 
 function PlantPage() {
+  // State
+  const [plants, setPlants] = useState([]);
 
-    // State
-    const [plants, setPlants] = useState([])
+  useEffect(() => {
+    axios
+      .get("https://watermyplantunit4.herokuapp.com/plants/plants")
+      .then((res) => {
+        setPlants(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    useEffect(() => {
-        axios.get('https://watermyplantunit4.herokuapp.com/plants/plants')
-            .then(res => {
-                setPlants(res.data)
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }, [])
-
-    return (
-        <>
-            <Plants plants={plants} />
-            <EditItem plants={plants} updatePlants={setPlants} />
-        </>
-    )
+  return (
+    <div className="parent">
+      <EditItem plants={plants} updatePlants={setPlants} />
+      <Plants plants={plants} />
+    </div>
+  );
 }
 
-export default PlantPage
+export default PlantPage;
