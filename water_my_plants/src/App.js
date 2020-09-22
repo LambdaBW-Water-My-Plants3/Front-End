d27d7ae29f3afb937ac5576588793901a496be4a
unknown <brettmcadams96@gmail.com>
2020-09-22 15:39:54 -0400

    save before pull

--- water_my_plants/src/App.js
+++ water_my_plants/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import PlantPage from './components/PlantPage'
+
 
 function App() {
   return (
     <div className="App">
-      <PlantPage />
     </div>
   );
 }
