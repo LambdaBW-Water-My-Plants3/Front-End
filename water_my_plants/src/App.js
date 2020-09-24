62cebb14ed1bc0961ae2c437e9c1ea5f9092af79
KindredSoul <jabriltb2@gmail.com>
2020-09-24 11:37:56 -0500

    changes after merge

--- water_my_plants/src/App.js
+++ water_my_plants/src/App.js
@@ -3,18 +3,13 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-<<<<<<< HEAD
 import { FormWrapper } from "./theme/muiStyles";
 import NavBar from "./components/NavBar";
 import Buttons from "./components/OnboardButtons";
-=======
-import { withStyles } from "@material-ui/styles";
-import { Box } from "@material-ui/core";
 import PrivateRoute from "./components/PrivateRoute";
 import PlantPage from "./components/PlantPage";
 import UserPage from "./components/UserPage";
-import Item from './components/Item'
->>>>>>> 8c0b0444c4aa398a3d131fbded29e6db583bab6e
+import Item from "./components/Item";
 
 function App() {
 	return (
@@ -32,8 +27,8 @@ function App() {
 							<SignUp />
 						</FormWrapper>
 					</Route>
-					<PrivateRoute exact path='/plantpage' component={PlantPage} />
-					<PrivateRoute exact path='/userpage' component={UserPage} />
+					<PrivateRoute exact path="/plantpage" component={PlantPage} />
+					<PrivateRoute exact path="/userpage" component={UserPage} />
 				</Switch>
 			</header>
 		</div>
--- water_my_plants/src/components/Login.js
+++ water_my_plants/src/components/Login.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import * as yup from "yup";
 import schema from "./login-validation/formSchema";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import styled from "styled-components";
 
 const StyledContainer = styled.div`
@@ -13,8 +12,27 @@ const StyledContainer = styled.div`
 	color: ${(props) => props.theme.secondaryColor};
 `;
 
-const StyledButtons = styled.button`
-	text-decoration: none;
+const StyledHeader = styled.h1`
+	color: ${(props) => props.theme.headerColor};
+`;
+
+const ErrorStyles = styled.div`
+	color: red;
+	font-size: 16px;
+`;
+
+function UsernameInfo() {
+	const username = document.getElementById("username");
+	username.style.backgroundColor = "lightblue";
+}
+
+function PasswordInfo() {
+	const username = document.getElementById("password");
+	username.style.backgroundColor = "lightblue";
+}
+
+const SignUp = styled.h4`
+	color: ${(props) => props.theme.secondaryColor};
 `;
 
 const initialValues = {
@@ -30,6 +48,8 @@ const initialErrors = {
 const initialDisabled = true;
 
 const Login = (props) => {
+	const history = useHistory();
+
 	const [credentials, setCredentials] = useState(initialValues);
 	const [credentialErrors, setCredentialErrors] = useState(initialErrors);
 	const [disabled, setDisabled] = useState(initialDisabled);
@@ -51,7 +71,7 @@ const Login = (props) => {
 			.then((res) => {
 				console.log(res.data);
 				localStorage.setItem("token", res.data.access_token);
-				props.history.push("/login");
+				history.push("/item");
 			});
 		setCredentials(initialValues);
 	};
@@ -94,11 +114,13 @@ const Login = (props) => {
 	return (
 		<StyledContainer className="loginContainer">
 			<form className="loginForm" onSubmit={login}>
-				<h1>Welcome Back</h1>
+				<StyledHeader>Welcome Back! 🌱</StyledHeader>
 				<div className="username">
 					<label>
 						Username:
 						<input
+							id="username"
+							onClick={UsernameInfo}
 							type="text"
 							name="username"
 							value={credentials.username}
@@ -110,6 +132,8 @@ const Login = (props) => {
 					<label>
 						Password:
 						<input
+							id="password"
+							onClick={PasswordInfo}
 							type="password"
 							name="password"
 							value={credentials.password}
@@ -118,173 +142,27 @@ const Login = (props) => {
 					</label>
 				</div>
 				<div className="errors">
-					<div>{credentialErrors.username}</div>
-					<div>{credentialErrors.password}</div>
+					<ErrorStyles className="errors">
+						{credentialErrors.username}
+					</ErrorStyles>
+					<ErrorStyles className="errors">
+						{credentialErrors.password}
+					</ErrorStyles>
 				</div>
 				<div className="buttons">
-					<StyledButtons disabled={disabled}>Login</StyledButtons>
+					<div>
+						<button disabled={disabled}>Login</button>
+					</div>
 					<p>Don't have an account?</p>
-					<StyledButtons>
-						<Link to="/signup">Sign Up Here</Link>
-					</StyledButtons>
+					<div>
+						<Link to="/signup">
+							<SignUp>Sign Up Here</SignUp>
+						</Link>
+					</div>
 				</div>
 			</form>
 		</StyledContainer>
 	);
 };
-=======
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
-import * as yup from 'yup'
-import schema from './login-validation/formSchema'
-import {Link} from 'react-router-dom' 
-import styled from 'styled-components'
-
-
-const StyledContainer = styled.div ` 
-background-color: ${props => props.theme.primaryColor};
-padding: 10%;
-border-radius: 10px;
-color: ${props => props.theme.secondaryColor};
-`
-
-const StyledHeader = styled.h1 ` 
-color: ${props => props.theme.headerColor};
-`
-
-const ErrorStyles = styled.div ` 
-color: red;
-font-size: 16px;
-`
-
-function UsernameInfo() {
-    const username = document.getElementById('username')
-    username.style.backgroundColor = 'lightblue'
-}
-
-function PasswordInfo (){
-    const username = document.getElementById('password')
-    username.style.backgroundColor = 'lightblue'
-}
-
-
-const SignUp = styled.h4 ` 
-color: ${props => props.theme.secondaryColor};
-`
-
-const initialValues = {
-  username: "",
-  password: "",
-};
-
-const initialErrors = {
-  username: "",
-  password: "",
-};
-
-const initialDisabled = true;
-
-const Login = (props) => {
-
-    const [credentials, setCredentials] = useState(initialValues);
-    const [credentialErrors, setCredentialErrors] = useState(initialErrors)
-    const [disabled, setDisabled] = useState(initialDisabled)
-
-
-     const login = e => {
-       e.preventDefault();
-       axios.post('https://watermyplantunit4.herokuapp.com/login', `grant_type=password&username=${credentials.username}&password=${credentials.password}`, {
-         headers: {
-           // btoa is converting our client id/client secret into base64
-           Authorization: `Basic ${btoa('lambda-client:lambda-secret')}`,
-           'Content-Type': 'application/x-www-form-urlencoded'
-         }
-       })
-       .then(res => {
-         console.log(res.data)
-         localStorage.setItem('token', res.data.access_token);
-         props.history.push('/item');
-       })
-       setCredentials(initialValues)
-     }
-     const inputChange = (name, value) => {
-         validate(name, value)
-         setCredentials({
-             ...credentials, [name]: value
-         })
-     }
-     const onChange = evt => {
-         const {name, value} = evt.target
-         inputChange(name, value)
-     }
-
-     const validate = (name, value) => {
-        yup
-        .reach(schema, name)
-        .validate(value)
-        .then(valid =>{
-            setCredentialErrors({
-                ...credentialErrors, [name]: ""
-            })
-        })
-        .catch(error =>{
-            setCredentialErrors({
-                ...credentialErrors, [name]:error.errors[0]
-            })
-        })
-     }
-
-     useEffect(()=>{
-         schema.isValid(credentials)
-         .then(valid =>{
-             setDisabled(!valid)
-         })
-     },[credentials])
-
-
-     return(
-        <StyledContainer className = "loginContainer">
-        <form className = "loginForm" onSubmit={login}>
-            <StyledHeader>Welcome Back! 🌱</StyledHeader>
-        <div className = "username">
-            <label>Username:
-                <input
-                    id = "username"
-                    onClick = {UsernameInfo}
-                    type="text"
-                    name="username"
-                    value = {credentials.username}
-                    onChange = {onChange}
-                />
-            </label>
-        </div>
-        <div className="password">
-            <label>Password:
-                <input
-                    id = "password"
-                    onClick = {PasswordInfo}
-                    type= "password"
-                    name="password"
-                    value={credentials.password}
-                    onChange = {onChange}
-                />
-            </label>
-        </div> 
-        <div className = "errors">
-            <ErrorStyles className = "errors">{credentialErrors.username}</ErrorStyles>
-            <ErrorStyles className = "errors">{credentialErrors.password}</ErrorStyles>
-        </div>
-        <div className="buttons">
-            <div><button disabled={disabled}>Login</button></div>
-            <p>Don't have an account?</p><div><Link to = "/signup"><SignUp>Sign Up Here</SignUp></Link></div>
-        </div>   
-        </form>
-        
-        </StyledContainer>
-     )
-     
-    }
-
->>>>>>> 8c0b0444c4aa398a3d131fbded29e6db583bab6e
 
 export default Login;
