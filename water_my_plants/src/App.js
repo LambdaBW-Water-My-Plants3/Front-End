bb5f320e920d07321102216aaaf035bc4284f7ec
unknown <brettmcadams96@gmail.com>
2020-09-24 15:05:22 -0400

    final updates

--- water_my_plants/src/App.js
+++ water_my_plants/src/App.js
@@ -3,18 +3,13 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-<<<<<<< HEAD
 import { FormWrapper } from "./theme/muiStyles";
 import NavBar from "./components/NavBar";
 import Buttons from "./components/OnboardButtons";
-=======
-import { withStyles } from "@material-ui/styles";
-import { Box } from "@material-ui/core";
 import PrivateRoute from "./components/PrivateRoute";
 import PlantPage from "./components/PlantPage";
 import UserPage from "./components/UserPage";
 import Item from './components/Item'
->>>>>>> 8c0b0444c4aa398a3d131fbded29e6db583bab6e
 
 function App() {
 	return (
